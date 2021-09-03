72ddb8ebf34582d876a0221c4fedd4060e4f242c
ℓєgєи∂-ℓx_ Háćķéŕ &【★༒Fīģhťéŕ <87700009+prawashmalviya@users.noreply.github.com>
2021-09-02 22:25:03 -0700

    Update stringsetup.py

--- stringsetup.py
+++ stringsetup.py
@@ -2,7 +2,7 @@ from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 print("")
-print("""Welcome To LEGENDBOT String Session\nGenerator By @LEGENDOwner\n\n""")
+print("""Welcome To PYTHONBOT String Session\nGenerator By @LEGEND-LXOwner\n\n""")
 print("""Enter Your Valid Details To Continue!\n\n """)
 
 API_KEY = input("API_ID:  ")
@@ -18,16 +18,16 @@ while True:
             session = client.session.save()
             client.send_message(
                 "me",
-                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it) \n\n `{session}` \n\n And Visit @LEGENDSupport For Any Help!\n\n",
+                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it) \n\n `{session}` \n\n And Visit @PYTHONSupport For Any Help!\n\n",
             )
 
             print(
-                "Thanks for Choosing LEGENDBOT Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
+                "Thanks for Choosing PYTHONBOT Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
             )
     except:
         print("")
         print(
-            "Wrong phone number \n make sure its with correct country code. Example : +918925534834! Kindly Retry"
+            "Wrong phone number \n make sure its with correct country code. Example : +918945136789! Kindly Retry"
         )
         print("")
         continue
