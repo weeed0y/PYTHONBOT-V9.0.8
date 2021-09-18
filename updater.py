7265ed01c2b5b5d4988d692854f41597fc62882b
【★༒✞︎Îτß-ℓєgєи∂-ℓx】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-09-17 21:59:29 -0700

    update

--- updater.py
+++ updater.py
@@ -42,7 +42,7 @@ async def update_requirements(main , test):
     try:
         for i in a:
             await runcmd(f"pip install {i}")
-            print(f"⚡𝕷êgëñd𝕭øt V2.O⚡ {i}")
+            print(f"🐍PYTHON𝕭øt V9.O.8🐍 {i}")
     except Exception as e:
         print(f"Error while installing requirments {str(e)}")
 
